ab0b4eb97a94b624f1a89841b74bc66c7b700cc2
abhi-drops <abhinand7667@gmail.com>
2024-07-23 00:41:09 +0530

    changes3 : bug-fix

--- signin.js
+++ signin.js
@@ -1,17 +1,23 @@
 // This is a JavaScript function that handles user sign-in functionality. It checks if the user exists in the localStorage based on the provided username, verifies the password, and redirects the user to the user.html page upon successful login.
 
 function signin() {
-  if (uacnum.value in localStorage) {
-    user = JSON.parse(localStorage.getItem(uacnum.value));
-    if (user.upass == upass.value) {
-      console.log( user.uacnum);
-      localStorage.setItem("ACTIVEUSER", user.uacnum);
-      alert("user successfully logined");
-      window.location.href = "./user.html";
+  if (uacnum.value == '' || upass.value == '') {
+    alert('Please enter your username and password!');
+  }else{
+    if (uacnum.value in localStorage) {
+      user = JSON.parse(localStorage.getItem(uacnum.value));
+      if (user.upass == upass.value) {
+        console.log( user.uacnum);
+        localStorage.setItem("ACTIVEUSER", user.uacnum);
+        alert("user successfully logined");
+        window.location.href = "./user.html";
+      } else {
+        alert("wrong password");
+      }
     } else {
-      alert("wrong password");
+      alert("wrong username");
     }
-  } else {
-    alert("wrong username");
   }
+
+
 }
--- signup.js
+++ signup.js
@@ -1,22 +1,26 @@
 
 
 function signup() {
-  if (uacnum.value in localStorage) {
-    alert("account number already in use ")
+  if (uacnum.value == '' || uname.value == '' || upass.value == '' ) {
+    alert("Please fill in all the fields");
   }else{
-    user = {
-      uacnum : uacnum.value,
-      uname : uname.value,
-      upass : upass.value,
-      ubal : 0
-  }
+    if (uacnum.value in localStorage) {
+      alert("account number already in use ")
+    }else{
+      user = {
+        uacnum : uacnum.value,
+        uname : uname.value,
+        upass : upass.value,
+        ubal : 0
+    }
 
-  localStorage.setItem(uacnum.value,JSON.stringify(user))
-      uacnum.value = ""
-      uname.value = ""
-      upass.value = ""
-      alert("User data successfully stored");
-      window.location = './signin.html';
+    localStorage.setItem(uacnum.value,JSON.stringify(user))
+        uacnum.value = ""
+        uname.value = ""
+        upass.value = ""
+        alert("User data successfully stored");
+        window.location = './signin.html';
+    }
   }
 
 }
